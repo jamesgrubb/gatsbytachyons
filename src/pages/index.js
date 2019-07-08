3937fa502fa4563868e30dfa93f1711d2ddcd2a7
jamesgrubb <jamesgrubb@mac.com>
2019-07-08 16:31:57 +0100

    tachyons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => <div className="fw9 f2 pa3 headline light-blue db fw vh-50 bg-blue flex items-center justify-center">Hello world!</div>
