edc730886e48019d108975bca62079db6ee2d5c9
jamesgrubb <jamesgrubb@mac.com>
2019-07-08 17:54:46 +0100

    attrs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import styled from "styled-components"
 
-const Hello = styled.div`
+const Hello = styled.div.attrs({
+    className: "flex items-center justify-center"
+})`
 border: 20px solid red;
 `
 
-export default () => <Hello className="fw9 f2 pa3 headline light-blue db fw vh-50 bg-blue flex items-center justify-center">Hello world!</Hello>
+export default () => <Hello className="fw9 f2 pa3 headline light-blue db fw vh-50 bg-blue">Hello world!</Hello>
